6506ffb041fe4abc0e53f5e7bb2c2365e65e32c1
Rosiane Silva Cruz <rosecruz82@gmail.com>
2021-12-28 09:05:23 -0300

    Iniciando o Git

---
+++ public/js/base.js
@@ -0,0 +1,79 @@
+const nome ="marcelo";
+let nome2 = "";
+let pessoaDefault = {
+
+    nome: "Marcelo Eltz",
+    idade: "33",
+    trabalho: "Programador"
+}
+
+let pessoasListaVazia= [];
+let nomes = ["marcelo eltz", "MAria Silva", "Pedro Silva"];
+ 
+
+function adcionarPessoa(pessoas){
+
+    pessoas.push(pessoas);
+}
+
+function adcionarPessoa ({
+
+    nome: "Pedro Silva",
+    idade: "28",
+    trabalho:"porteiro"
+
+});
+
+console.log(pessoas);
+
+//console.log(nomes);
+//console.log (nomes[1]);
+
+//console.log(pessoas[1])
+
+//function imprimirPessoa (pessoa) {
+    
+    //console.log("Nome:");
+    //console.log (pessoa.nome);
+
+  //  console.log("Idade:");
+   // console.log(pessoa.idade);
+
+    //console.log ("Profissão:");
+   //console.log (pessoa.trabalho);
+
+
+
+//}
+
+//imprimirPessoa(pessoaDefault);
+
+//imprimirPessoa ({
+
+    //nome: "Maria Silva",
+    //idade: "25",
+   // profissão: "Ux/Ui Designer"
+//});
+
+
+
+//function alterarNome(){
+
+//        nome2 ="Maria Silva";
+//console.log("Valor Alterado:");
+//console.log(nome2);
+//}
+
+//function recebeEalteraNome(novoNome){
+  //  nome2= novoNome;
+
+   // console.log("Nome Alterado Recebendo um Nome:");
+   // console.log(nome2);
+
+//}
+
+
+//recebeEalteraNome("Rose Cruz");
+//recebeEalteraNome("Maria Silva")
+
+//alterarNome();
